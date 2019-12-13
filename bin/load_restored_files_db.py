571d03612beeed9a9e952cd1f36039bff0f46ff2
Joshua Westgard <jwestgard@mac.com>
2019-12-13 18:32:49 -0500

    restored files loader work

--- bin/load_restored_files_db.py
+++ bin/load_restored_files_db.py
@@ -2,6 +2,7 @@
 
 import csv
 import os
+import re
 import sys
 
 class RestoredFileList():
@@ -13,16 +14,30 @@ class RestoredFileList():
     def __init__(self, path):
         self.filename = os.path.basename(path)
         self.batchname = self.filename[59:-14]
-        print(self.batchname)
-
+        self.id = self.get_identifier()
         with open(path) as handle:
             reader = csv.reader(handle)
+            self.contents = []
             for row in reader:
                 asset = Asset(*row)
+                self.contents.append(asset)
+
+    def size(self):
+        """Return the sum of the bytes for all assets in the list."""
+        return sum([asset.bytes for asset in self.contents])
+
+    def get_identifier(self):
+        """Attempt to extract an id of form 'batch_mdu_name' from batchname."""
+        pattern = r'batch[ _]mdu[ _]\w+'
+        match = re.search(pattern, self.batchname)
+        if match:
+            return match.group(0).replace(' ', '_')
+        else:
+            return "batch_mdu"
 
 
 class Asset():
-    """Class representing an individual asset record"""
+    """Class representing an individual asset record."""
 
     def __init__(self, md5, path, filename, bytes):
         self.md5 = md5
@@ -31,6 +46,7 @@ class Asset():
         self.bytes = int(bytes)
         
 
-for file in os.listdir(sys.argv[1])[:5]:
+for file in os.listdir(sys.argv[1]):
     path = os.path.join(sys.argv[1], file)
     rfl = RestoredFileList(path)
+    print(f"{rfl.id}\t{len(rfl.contents)}\t{rfl.size()}\t{rfl.filename}")
